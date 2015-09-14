83e42b6f116ff800dc03b3fa0e3c62a68701e898
gitsMachine <jeschror@gmail.com>
2015-09-14 18:59:41 -0400

    Removed unneeded signals.py

--- app/users/signals.py
+++
@@ -1,9 +0,0 @@
-from django.db.models.signals import post_save
-
-def update_referrals(sender, **kwargs):
-	# print 'We got it: ', instance
-	print ' and also: ', kwargs['instance']
-	# if referrer:
-	# 	user = User.objects.get(referral_code=ref)
-	# 	user.referrals = user.referrals + 1
-	# 	user.save()
